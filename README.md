ce6d6e6d4d8c21f3f215be2852c2139593b2e07d
Jonathan Hudak <hudak.jonathan@gmail.com>
2022-09-10 10:12:40 -0700

    Init Fresh 1.1 with @preact/signals

---
+++ README.md
@@ -0,0 +1,17 @@
+# Breathing
+
+[![Made with Fresh](https://fresh.deno.dev/fresh-badge.svg)](https://fresh.deno.dev)
+
+### Usage
+
+Start the project:
+
+```
+deno task start
+```
+
+This will watch the project directory and restart as necessary.
+
+## Fresh
+
+- https://deno.com/blog/fresh-1.1
---
+++ islands/Counter.tsx
@@ -0,0 +1,17 @@
+import { useSignal } from "@preact/signals";
+import { Button } from "../components/Button.tsx";
+
+interface CounterProps {
+  start: number;
+}
+
+export default function Counter(props: CounterProps) {
+  const count = useSignal(props.start);
+  return (
+    <div class="flex gap-2 w-full">
+      <p class="flex-grow-1 font-bold text-xl">{count}</p>
+      <Button onClick={() => (count.value -= 1)}>-1</Button>
+      <Button onClick={() => (count.value += 1)}>+1</Button>
+    </div>
+  );
+}
